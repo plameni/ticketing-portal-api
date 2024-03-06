904b4a20e37aa150f388db8c38fd78d3504fc6be
Aleksandar Plamenac <plamenac.a@ucg.ac.me>
2024-03-06 14:25:47 +0100

    initial commit

---
+++ src/main/java/com/ticketing/portal/TicketingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingPortalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ticketing/portal/repositories/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.ticketing.portal.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProjectRepository {
+
+    // SQL UPITI
+}
